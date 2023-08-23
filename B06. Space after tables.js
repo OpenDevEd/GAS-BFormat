6f0d32c82562435f92140069d022acc781a36f55
Elena Goretskaia <elenaliftup@gmail.com>
2023-08-23 14:30:15 +0600

    Updated setSpaceAfterTables20pt (Tables inside cells bug)

--- B06. Space after tables.js
+++ B06. Space after tables.js
@@ -3,16 +3,20 @@ function setSpaceAfterTables20pt(body = DocumentApp.getActiveDocument().getBody(
   let childIndexTable, parAfterTable, parText;
   const tables = body.getTables();
   for (let i = 1; i < tables.length; i++) {
-    childIndexTable = body.getChildIndex(tables[i]);
-    parAfterTable = body.getChild(childIndexTable + 1);
-    if (parAfterTable.getType() == DocumentApp.ElementType.PARAGRAPH) {
-      parText = parAfterTable.asText().getText();
-      if (parText != '') {
-        body.insertParagraph(childIndexTable + 1, '').setHeading(DocumentApp.ParagraphHeading.NORMAL).setSpacingAfter(8);
-      } else {
-        parAfterTable.asParagraph().setHeading(DocumentApp.ParagraphHeading.NORMAL).setSpacingAfter(8);
+    const parentType = tables[i].getParent().getType();
+    if (parentType == 'BODY_SECTION') {
+      //Logger.log('tables[i].getText()= %s', tables[i].getText());
+      childIndexTable = body.getChildIndex(tables[i]);
+      //Logger.log('childIndexTable= %s', childIndexTable);
+      parAfterTable = body.getChild(childIndexTable + 1);
+      if (parAfterTable.getType() == DocumentApp.ElementType.PARAGRAPH) {
+        parText = parAfterTable.asText().getText();
+        if (parText != '') {
+          body.insertParagraph(childIndexTable + 1, '').setHeading(DocumentApp.ParagraphHeading.NORMAL).setSpacingAfter(8);
+        } else {
+          parAfterTable.asParagraph().setHeading(DocumentApp.ParagraphHeading.NORMAL).setSpacingAfter(8);
+        }
       }
-
     }
   }
 }
